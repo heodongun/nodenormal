c113353ab4a526206ba6a49fdad9ff184d71e6d0
heodongun <heodongun0922@gmail.com>
2024-07-18 10:13:08 +0900

    last

--- server.js
+++ server.js
@@ -8,6 +8,9 @@ app.use(express.urlencoded({ extended: true }));
 const session = require('express-session');
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
+let quest1;
+let quest2;
+let quest3;
 
 app.use(session({
   secret: '암호화에 쓸 비번',
@@ -71,7 +74,8 @@ app.get('/', async (request, respond) => {
 app.get('/heart', async (request, respond) => {
   heart = request.user ? request.user.heart : null;
   heart = parseInt(heart);
-  respond.render('myInfo.ejs', { username: request.user ? request.user.username : null, heart: heart });
+  let other=request.user ? '('+request.user.other+')' : null;
+  respond.render('myInfo.ejs', { username: request.user ? request.user.username : null, heart: heart ,other:other});
 });
 
 app.post('/login', async (request, respond, next) => {
@@ -102,11 +106,19 @@ app.post('/logout', (req, res) => {
   });
 });
 
-app.get('/mission',(request,respond)=>{
+app.get('/mission',async (request,respond)=>{
+  quest = await db.collection('post').findOne({ _id: new ObjectId('66984c0464e2af1a37a01fe2') });
+  quest=parseInt(quest.number)
+  quest1 = await db.collection('post').findOne({ id: String(quest) });
+  quest2=await db.collection('post').findOne({ id: String(quest+1) });
+  quest3=await db.collection('post').findOne({id:String(quest+2)});
+  console.log(quest1.title)
+  console.log(quest2.title)
+  console.log(quest3.title)
   today = request.user ? request.user.today : null;
   today=parseInt(today)
   if(today==0){
-    respond.render('quest.ejs', { username: request.user ? request.user.username : null });
+    respond.render('quest.ejs', { username: request.user ? request.user.username : null,quest1:quest1.title,quest2:quest2.title,quest3:quest3.title});
   }
   else if(today==1){
     respond.render('complete.ejs', { username: request.user ? request.user.username : null});
@@ -117,17 +129,34 @@ app.get('/mission',(request,respond)=>{
 })
 
 app.post('/saveScore', async (req, res) => {
+  
   let score = req.body.score;
-  const userId = req.user._id;
-  let result = await db.collection('user').findOne({ _id: new ObjectId(userId) });
-  result = parseInt(result.heart);
+  const username = req.user.other;
+  let result = await db.collection('user').findOne({ username: username });
+  let result1=result.today;
+  result2 = parseInt(req.user.heart);
+  result3=parseInt(result.heart)
   score = parseInt(score);
   if (score == 3) {
-      result = String(result + 1);
+    if(parseInt(result.today)==1){
+      result2 = String(result2 + 1);
+      console.log(result3)
+      result3 = String(result3 + 1);
       await db.collection('user').updateOne(
-          { _id: new ObjectId(userId) },
-          { $set: { heart: result, today: "1" } }
+          { _id: new ObjectId(req.user._id) },
+          { $set: { heart: result2, today: "1" } }
       );
+      await db.collection('user').updateOne(
+        { username: result.username },
+        { $set: { heart: result3} }
+    );
+    }
+    else{
+      await db.collection('user').updateOne(
+        { _id: new ObjectId(req.user._id) },
+        { $set: { today: "1" } }
+    );
+    }
   }
   res.redirect('/')
 });
